72c0e4c0a52dfea256caa241560d61a628a5a5ac
jagadeeshjaggu2001 <jagadeeshjaggu002@gmail.com>
2023-04-03 15:54:44 +0530

    first commit

---
+++ modules.js
@@ -0,0 +1,18 @@
+import  personXYZ from "./modules.js";
+
+import {Person2 as test} from "./personmodules.js";
+
+import {Person2 as number} from "./test.js";
+
+
+
+class Student extends personXYZ {
+
+}
+
+const student = new Student(10,"wertyu","m","Full stack","cs");
+const person = new personXYZ(10, "asdfg","f");
+
+export default Person;
+
+console.log(student)
---
+++ personmodules.js
@@ -0,0 +1,32 @@
+class Person {
+    constructor(_age,_name,_gender){
+       this.age = _age;
+       this.name = _name;
+       this.gender = _gender;
+    }
+
+    getDOB = () =>{
+        return new Date ();
+    }
+}
+
+class Person2 {
+    constructor(_age,_name,_gender){
+       this.age = _age;
+       this.name = _name;
+       this.gender = _gender;
+    }
+
+    getDOB = () =>{
+        return new Date ();
+    }
+}
+
+const animal = {
+    name : "djsgh",
+    age : 10
+
+}
+
+
+
